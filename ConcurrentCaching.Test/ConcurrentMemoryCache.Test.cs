54448950a7df76a06eea101e09264454ff2a0bb5
Oscar Yu <yuszy@outlook.com>
2021-11-17 00:18:48 +1100

    fix concurrent errors in tests, include readme in package

--- ConcurrentCaching.Test/ConcurrentMemoryCache.Test.cs
+++ ConcurrentCaching.Test/ConcurrentMemoryCache.Test.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Xunit;
@@ -7,6 +9,31 @@ namespace ConcurrentCaching.Test
 {
     public class ConcurrentMemoryCacheTests
     {
+        [Fact]
+        public async void BenchmarkConcurrentFetchWithDifferentEntries()
+        {
+            var cacheEntryOptions = new MemoryCacheOptions();
+            var cache = new ConcurrentMemoryCache(new MemoryCache(cacheEntryOptions));
+            var tasks = new List<Task>();
+
+            var timer = Stopwatch.StartNew();
+
+            for (var i = 0; i < 200000; i++)
+            {
+                tasks.Add(Task.Run(() => cache.GetOrCreateAsync<int>(Guid.NewGuid().ToString(), async entry =>
+                {
+                    await Task.Delay(400);
+                    return i;
+                })));
+            }
+
+            await Task.WhenAll(tasks);
+
+            timer.Stop();
+
+            Assert.True(timer.Elapsed.TotalMilliseconds < 2000);
+        }
+
         [Fact]
         public async void FetchWithSingleEntry()
         {
@@ -17,7 +44,7 @@ namespace ConcurrentCaching.Test
 
             for (var i = 0; i < 10; i++)
             {
-                tasks.Add(Task.Run(() => results.Add(cache.GetOrCreate<int>("key", entry => i))));
+                tasks.Add(Task.Run(() => results.Add(cache.GetOrCreate<int>("key1", entry => i))));
             }
 
             await Task.WhenAll(tasks);
@@ -36,7 +63,7 @@ namespace ConcurrentCaching.Test
 
             for (var i = 0; i < 10; i++)
             {
-                tasks.Add(Task.Run(() => cache.GetOrCreateAsync("key", async entry =>
+                tasks.Add(Task.Run(() => cache.GetOrCreateAsync("key2", async entry =>
                 {
                     await Task.Delay(100 - i * 5);
                     return i;
@@ -66,7 +93,7 @@ namespace ConcurrentCaching.Test
             {
                 tasks.Add(Task.Run(() =>
                 {
-                    var res = cache.GetOrCreate($"key {i}", entry => i);
+                    var res = cache.GetOrCreate($"key1-{i}", entry => i);
                     Assert.Equal(i, res);
                 }));
             }
@@ -84,7 +111,7 @@ namespace ConcurrentCaching.Test
             {
                 tasks.Add(Task.Run(async () =>
                 {
-                    var res = await cache.GetOrCreateAsync($"key {i}", entry => Task.FromResult(i));
+                    var res = await cache.GetOrCreateAsync($"key2-{i}", entry => Task.FromResult(i));
                     Assert.Equal(i, res);
                 }));
             }
